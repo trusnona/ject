4ed6c607b27f2e1daff188f3d71ac2289f2801a5
Trat Siriyothin <trus_siri@hotmail.com>
2017-06-10 12:20:05 +0700

    Update Ject.java

--- src/ject/Ject.java
+++ src/ject/Ject.java
@@ -55,7 +55,8 @@ class Menu{
         
         Case c = new Case();
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please select the menu : ");
+        System.out.println("Please select the menu : "+"/n");
+        System.out.println("1.menu1"+"/n"+"2.menu2"+"/n"+"3.menu3"+"/n"+"4.menu4"+"/n"+"5.menu5"+"/n"+"6.exit");
         
         int menu = scanner.next().charAt(0);
          
